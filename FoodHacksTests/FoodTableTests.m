b35d5c0575bedf1c5283fd3f0b0ee9a46b298b97
Rachel Dorn <rachel.m.dorn@gmail.com>
2016-08-05 18:37:35 -0400

    display food name in detail view

---
+++ FoodHacksTests/FoodTableTests.m
@@ -0,0 +1,33 @@
+//
+//  FoodTableTests.m
+//  FoodHacks
+//
+//  Created by Rachel Dorn on 8/5/16.
+//  Copyright © 2016 Rachel Dorn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "RMDFoodTableViewController.h"
+
+@interface FoodTableTests : XCTestCase
+
+@end
+
+@implementation FoodTableTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testDidSelectRow {
+    RMDFoodTableViewController *tableView = [[RMDFoodTableViewController alloc] init];
+    
+}
+
+@end
